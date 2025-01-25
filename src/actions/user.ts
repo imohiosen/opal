019388d86f83a072fec4b88214512c888b1de677
Abraham Imohiosen <abrahamimohiosen@gmail.com>
2025-01-25 11:13:02 +0100

    initial commit

---
+++ src/actions/user.ts
@@ -0,0 +1,22 @@
+'use server'
+
+import { currentUser } from "@clerk/nextjs/server"
+
+export const onAuthenticateUser = async () => {
+    try {
+        // Authenticate user
+        const user = await currentUser();
+        if (!user) {
+            return {
+                status: 403,
+            };
+        }
+        // const existingUser = await db.user.findUnique({
+        //     where: {
+        //         clerkId: user.id,
+        //     },
+        // });
+    } catch (error) {
+        // Handle error
+    }
+}
